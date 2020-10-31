90de96d82ab4508652767f213220eaaeb92497e0
thisisstreamliner <thisisstreamliner@gmail.com>
2020-10-31 11:47:37 +0200

    Building in Functionality and Managing State

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -9,6 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -18,14 +22,15 @@ struct ContentView: View {
                 Image("diceeLogo")
                 Spacer()
                 HStack {
-                    DiceView(n: 1)
-                    DiceView(n: 2)
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
                 }
                 .padding(.horizontal)
                 Spacer()
                 Button(
                     action: {
-                
+                        self.leftDiceNumber = Int.random(in: 1...6)
+                        self.rightDiceNumber = Int.random(in: 1...6)
                 }){
                 Text("Roll")
                     .font(.system(size:50))
@@ -34,6 +39,7 @@ struct ContentView: View {
                     .padding(.horizontal)
                 }
                 .background(Color.red)
+                .padding(.bottom)
             }
         
         }
